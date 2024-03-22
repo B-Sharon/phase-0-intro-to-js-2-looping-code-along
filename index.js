9d567ca9a07ca976eeb022087acc099b680843e2
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-22 07:31:10 +0300

    completed the tasks

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names){
+    let cards = []; //new array to be returned
+    for(let i =0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countDown(number){
+    number = 0;
+    while(number <= 10){
+        console.log(number++);
+    } 
+}
